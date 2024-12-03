a43423137e825faea57dc180ddced8db5b8b739b
Santino Onyeme <santino.onyeme@hotmail.co.uk>
2024-12-03 16:44:57 +0000

    Change reading method to ReadString in FloatReader

--- internal/mortgage/input.go
+++ internal/mortgage/input.go
@@ -26,16 +26,20 @@ func IntReader(prompt string, reader *bufio.Reader) int {
 	}
 }
 
-func FloatReader(prompt string) float64 {
+func FloatReader(prompt string, reader *bufio.Reader) float64 {
 	for {
-		var value float64
 		fmt.Print(prompt)
-		_, err := fmt.Scanln(&value)
-		if err != nil || value <= 0 {
+    input, err := reader.ReadString('\n')
+		if err != nil {
 			fmt.Println("Invalid input. Please try again")
 			continue
 		}
-		return value
+    input = strings.TrimSpace(input)
+    result, err := strconv.ParseFloat(input, 64)
+    if err != nil {
+      fmt.Println("Error parsing input: ", err)
+    }
+		return result 
 
 	}
 }
@@ -43,8 +47,8 @@ func FloatReader(prompt string) float64 {
 func NewPayment() *Payment {
   reader := bufio.NewReader(os.Stdin)
 	p := &Payment{}
-	p.LoanPrincipal = FloatReader("Enter the loan principal (e.g., 100000): ")
-	p.InterestRate = FloatReader("Enter the annual interest rate (e.g., 5): ")
+	p.LoanPrincipal = FloatReader("Enter the loan principal (e.g., 100000): ", reader)
+	p.InterestRate = FloatReader("Enter the annual interest rate (e.g., 5): ", reader)
 	p.LoanTerm = IntReader("Enter the loan term in years (e.g., 30): ", reader )
 	fmt.Printf("\nSummary:\nLoan Principal: £%.2f\nInterest Rate: %.2f%%\nLoan Term: %d years\n\n", p.LoanPrincipal, p.InterestRate, p.LoanTerm)
 	return p
